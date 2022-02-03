7d182766c3f03de02bf774709e04c78f823670d9
Kritika Naagar <kritikanaagar15@gmail.com>
2022-02-03 09:08:43 +0530

    back

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 
-app.use(express.json())
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
 const port = process.env.PORT || 3000
